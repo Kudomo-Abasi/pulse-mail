2cfe41acd48c2ab0ee55a83928f171cb458c40f2
Kudomo-Abasi <kuyana57@gmail.com>
2024-02-21 11:19:45 +0200

    api updated

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
 // api.js
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://backend-pulse-mail.onrender.com";
+// const API_BASE_URL = "http://localhost:5000/api";
 
 export const AUTH_ENDPOINTS = {
   LOGIN: `${API_BASE_URL}/auth/login`,
